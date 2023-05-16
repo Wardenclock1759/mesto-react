4970afdace236ab07d922496e7217700694a7ee3
Misha Kukarkin <wardenclock@gmail.com>
2023-05-16 22:44:19 +0500

    minor fix

--- src/components/AddPlacePopup.jsx
+++ src/components/AddPlacePopup.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
+import { useForm } from '../hooks/useForm';
 
 function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
-    const [name, setName] = useState('');
-    const [link, setLink] = useState('');
+
+    const {values, handleChange, setValues} = useForm({ name: '', link: '' });
 
     useEffect(() => {
         if (isOpen) {
-            setName('');
-            setLink('');
+            setValues({ name: '', link: '' });
         }
-    }, [isOpen]);
+    }, [isOpen, setValues]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        onAddPlace({ name, link });
+        onAddPlace(values);
     }
 
     return (
         <PopupWithForm
             title="Новое место"
             name="photo"
+            buttonText="Создать"
             isOpen={isOpen}
             onClose={onClose}
             onSubmit={handleSubmit}
         >
-            <label for="title-input" class="popup__label">
-                <input type="text" class="popup__input" id="title-input" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Название" minlength="2" maxlength="30" required />
-                <span class="popup__input-error title-input-error"></span>
+            <label htmlFor="title-input" className="popup__label">
+                <input type="text" className="popup__input" id="title-input" name="name" value={values.name} onChange={handleChange} placeholder="Название" minLength="2" maxLength="30" required />
+                <span className="popup__input-error title-input-error"></span>
             </label>
-            <label for="url-input" class="popup__label">
-                <input type="url" class="popup__input" id="url-input" name="link" value={link} onChange={(e) => setLink(e.target.value)} placeholder="Ссылка на картинку" required />
-                <span class="popup__input-error url-input-error"></span>
+            <label htmlFor="url-input" className="popup__label">
+                <input type="url" className="popup__input" id="url-input" name="link" value={values.link} onChange={handleChange} placeholder="Ссылка на картинку" required />
+                <span className="popup__input-error url-input-error"></span>
             </label>
-            <button class="popup__button" type="submit">Создать</button>
         </PopupWithForm>
     );
 }
